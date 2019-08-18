d3ec788f54a93c018dfe4cc12a20e2535c918b32
王江 <wangjiang@wangjiangdeMacBook-Pro.local>
2019-08-18 19:41:34 +0800

    Feat . add annotaion processor options
    Signed-off-by: 王江 <wangjiang7747@gmail.com>

--- comment-compiler/src/main/java/com/comment/compiler/helper/FileHelper.java
+++ comment-compiler/src/main/java/com/comment/compiler/helper/FileHelper.java
@@ -7,13 +7,18 @@ import java.util.Map;
 
 import javax.annotation.processing.Filer;
 
+/**
+ * 生成文档的接口
+ *
+ * @author wangjiang
+ */
 public interface FileHelper {
 
     /**
      * 将类或方法，注释写成 Java 文件
      *
-     * @param filer     操作文件 {@link Filer}
-     * @param data      数据
+     * @param filer    操作文件 {@link Filer}
+     * @param data     数据
      * @param fileName 生成的 Java 文件的类名
      */
     void writeToFile(Filer filer, Map<String, List<List<CommentModel>>> data, String fileName) throws Exception;
